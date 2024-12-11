49a7ffa42dc65c8bf2b87efeab1b6ccb98fd4282
Udit Kumar Nandy <uditkumarnandy312@gmail.com>
2024-12-11 23:05:29 +0530

    Added fetch all users api

--- src/user-management/user-management.controller.js
+++ src/user-management/user-management.controller.js
@@ -14,3 +14,18 @@ export const fetchUserDetails = async (req, res) => {
     return res.status(400).json({ message: "Something went wtong" });
   }
 };
+
+export const fetchAllUsers = async (req, res) => {
+  try {
+    const user_id = req.user_id;
+    let name = (req.query.name || "") + "%";
+    const [data] = await db.query(
+      "SELECT USER_ID,NAME FROM USERS WHERE USER_ID!=? AND LOWER(NAME) LIKE LOWER(?)",
+      [user_id, name]
+    );
+    return res.json({ data: data });
+  } catch (e) {
+    console.log("Error is", e);
+    return res.json({ message: "Something went wrong" });
+  }
+};
--- src/user-management/user-management.routes.js
+++ src/user-management/user-management.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyToken } from "../auth-management/auth-management.controller.js";
-import { fetchUserDetails } from "./user-management.controller.js";
+import {
+  fetchAllUsers,
+  fetchUserDetails,
+} from "./user-management.controller.js";
 
 const userManagementRoute = express.Router();
 
 userManagementRoute.get("/userDetails", verifyToken, fetchUserDetails);
+userManagementRoute.get("/getAllUsers", verifyToken, fetchAllUsers);
 
 export default userManagementRoute;
